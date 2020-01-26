8447967feed5fc89678672cf9ef8938be6f2bbc0
Matthew Sullivan <contact@matthewsullivan.media>
2020-01-25 20:11:35 -0500

    Add breakpoint

--- src/App.js
+++ src/App.js
@@ -252,6 +252,20 @@ class App extends React.Component {
   };
 
   /**
+   * Component Did Mount
+   */
+  componentDidMount() {
+    window.addEventListener('resize', this._handleResize);
+  }
+
+  /**
+   * Component Did Unmount
+   */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._handleResize);
+  }
+
+  /**
    * Render
    * @return {jsx}
    */
@@ -554,6 +568,15 @@ class App extends React.Component {
    */
   _handleListItemClick = (index) => {
     this.setState({selectedIndex: index || 0});
+  };
+
+  /**
+   * Handle Resize
+   */
+  _handleResize = () => {
+    if (window.innerWidth < 1200) {
+      this.setState({open: false});
+    }
   };
 
   /**
