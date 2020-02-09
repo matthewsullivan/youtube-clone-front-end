7fdb14f3c7e5cbf81c3de58a0d3f30dd9a5fd656
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-08 20:11:54 -0500

    Add card avatars

--- src/components/elements/VideoThumbnail.js
+++ src/components/elements/VideoThumbnail.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import {makeStyles} from '@material-ui/core/styles';
@@ -14,6 +16,11 @@ const useStyles = makeStyles({
     borderRadius: 0,
     boxShadow: 'none',
     color: '#fff',
+  },
+
+  content: {
+    paddingLeft: 0,
+    parringRight: 0,
   },
 
   media: {
@@ -32,15 +39,20 @@ export default function VideoThumbail() {
           image="https://picsum.photos/200"
           title="Video Thumbnail"
         />
-        <CardContent>
-          <Typography component="h2" gutterBottom variant="h5">
-            Video title goes here
-          </Typography>
-          <Typography component="p" variant="body2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam
-          </Typography>
+        <CardContent className={classes.content}>
+          <Grid container spacing={2} wrap="nowrap">
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item>
+              <Typography component="p" gutterBottom>
+                Video title goes here
+              </Typography>
+              <Typography component="p" variant="body2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </Typography>
+            </Grid>
+          </Grid>
         </CardContent>
       </CardActionArea>
     </Card>
